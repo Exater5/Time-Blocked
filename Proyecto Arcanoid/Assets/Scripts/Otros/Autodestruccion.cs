27840ca816131f15286eb65349ecd8dd434c24b8
torcu <45042365+Exater5@users.noreply.github.com>
2019-02-02 22:46:48 +0100

    Añado Autodestruccion de Particulas

--- Proyecto Arcanoid/Assets/Scripts/Otros/Autodestruccion.cs
+++ Proyecto Arcanoid/Assets/Scripts/Otros/Autodestruccion.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Autodestruccion : MonoBehaviour
 {
+    public float tiempoVida = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Autodestruye", 2f);
+        Invoke("Autodestruye", tiempoVida);
     }
 
     // Update is called once per frame
