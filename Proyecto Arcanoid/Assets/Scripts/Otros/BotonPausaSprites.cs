ccd92e855cac1f087a68112aa879af9c03d992e0
torcu <45042365+Exater5@users.noreply.github.com>
2019-02-03 04:40:33 +0100

    BugPausa + Cambio de Icono

---
+++ Proyecto Arcanoid/Assets/Scripts/Otros/BotonPausaSprites.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonPausaSprites : MonoBehaviour
+{
+    Image imagen;
+    public Sprite iconoPausa;
+    public Sprite iconoPlay;
+    bool pulsas = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imagen = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AlPulsar()
+    {
+        pulsas = !pulsas;
+
+        if(pulsas == true)
+        {
+            imagen.sprite = iconoPlay;
+        }
+        else
+        {
+            imagen.sprite = iconoPausa;
+        }
+    }
+}
