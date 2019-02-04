48c12cf2c26fe43e8c841b9a84d17dba0717a2ef
exater5 <pablotorcuat@gmail.com>
2019-02-04 14:00:37 +0100

    Contador de Bolas

---
+++ Arkanoid_BugPausaFixed/Proyecto Arcanoid/Assets/Scripts/Barra/IgnoraColisiones.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoraColisiones : MonoBehaviour
+{
+    GameObject bola;
+    public GameObject[] bolas;
+    public int numeroBolas;
+    public int bolasInicio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bolas = GameObject.FindGameObjectsWithTag("Bola");
+        for (int i = 1; i < numeroBolas; ++i)
+        {
+           
+        }
+        numeroBolas = bolas.Length;
+    }
+}
